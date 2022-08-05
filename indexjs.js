e1c4385de57351fa11297631406aa83ce7bb90db
Sourav Chakraborty <sourav.992001@gmail.com>
2022-08-05 17:17:15 +0530

    initial commit

---
+++ indexjs.js
@@ -0,0 +1,161 @@
+togglehide1 = () =>
+{
+    id1 = document.getElementById('Str');
+    if(id1.style.display != 'none')
+        id1.style.display = 'none';
+    else
+        id1.style.display = 'block';
+}
+
+togglehide2 = () =>
+{
+    id2 = document.getElementById('Sot');
+    if(id2.style.display != 'none')
+        id2.style.display = 'none';
+    else
+        id2.style.display = 'block';
+}
+togglehide3 = () =>
+{
+    id3 = document.getElementById('Geo');
+    if(id3.style.display != 'none')
+        id3.style.display = 'none';
+    else
+        id3.style.display = 'block';
+}
+togglehide4 = () =>
+{
+    id3 = document.getElementById('Netw');
+    if(id3.style.display != 'none')
+        id3.style.display = 'none';
+    else
+        id3.style.display = 'block';
+}
+tg1 = () =>
+{
+    id = document.getElementById('ns');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg2 = () =>
+{
+    id = document.getElementById('kmp');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg3 = () =>
+{
+    id = document.getElementById('rk');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg4 = () =>
+{
+    id = document.getElementById('bs');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg5 = () =>
+{
+    id = document.getElementById('ss');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg6 = () =>
+{
+    id = document.getElementById('is');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg7 = () =>
+{
+    id = document.getElementById('qs');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg8 = () =>
+{
+    id = document.getElementById('ms');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg9 = () =>
+{
+    id = document.getElementById('hs');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg10 = () =>
+{
+    id = document.getElementById('or');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg11 = () =>
+{
+    id = document.getElementById('in');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg12 = () =>
+{
+    id = document.getElementById('gs');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg13 = () =>
+{
+    id = document.getElementById('jm');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg14 = () =>
+{
+    id = document.getElementById('fl');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg15 = () =>
+{
+    id = document.getElementById('wl');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
+tg16 = () =>
+{
+    id = document.getElementById('ff');
+    if(id.style.display != 'none')
+        id.style.display = 'none';
+    else
+        id.style.display = 'block';
+}
